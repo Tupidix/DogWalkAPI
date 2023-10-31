84744c32ba7c5230db3aa9f938b9abefc8c7a4ef
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2023-10-31 16:10:48 +0100

    send COOL sur index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("MDRLOL!");
+  res.send("COOL");
 });
 
 export default router;
