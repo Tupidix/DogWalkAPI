cfc0ebc381b01e74ad1d1e188991e4a19162afe6
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2023-11-03 16:25:32 +0100

    create schema in app.js

--- app.js
+++ app.js
@@ -3,6 +3,51 @@ import createError from "http-errors";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/DogWalkAPI');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema  ({
+username: String,
+firstname: String,
+lastname: String,
+email: String,
+password: String,
+birthdate: Date,
+isAdmin: {type: Boolean, default: false},
+localisation: {
+  type: {type: String},
+  coordinates: [],
+},
+dogList: [{type: Schema.Types.ObjectId, ref: 'Dog'}],
+currentPath: {type: Schema.Types.ObjectId, ref: 'Walk', default: 0},
+});
+
+mongoose.model('User', userSchema);
+
+const dogSchema = new Schema  ({
+  name: String,
+  birthdate: Date,
+  breed: String,
+  master: [{type: Schema.Types.ObjectId, ref: 'User'}],
+  dislike: [{type: Schema.Types.ObjectId, ref: 'Dog'}],
+  picture: {type: String},
+});
+
+mongoose.model('Dog', dogSchema);
+
+const walkSchema = new Schema  ({
+title: String,
+path: {
+  type: {type: String},
+  coordinates: [],
+},
+creator: {type: Schema.Types.ObjectId, ref: 'User'},
+});
+
+mongoose.model('Walk', walkSchema);
 
 const app = express();
 
