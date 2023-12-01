95593b36190eed704debe1669869a0978a7034bb
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-12-01 19:18:56 +0100

    Change welcome message on index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-	res.send("Hello World!");
+	res.send(
+		"Welcome to our API, please refer to the documentation to use it properly, go to /api-docs to see it"
+	);
 });
 
 export default router;
